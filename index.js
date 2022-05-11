e9812ac69a935e39430b4336f2f7d5351ce9c4bb
kaushik <kaushiksheel9@gmail.com>
2022-05-11 11:02:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let searchBtn = document.getElementById("btn");
+
+function getInput() {
+  let input = document.getElementById("input");
+  input.addEventListener("input", (e) => {
+    fetchData(e.target.value);
+  });
+}
+
+getInput();
+
+searchBtn.addEventListener("click", () => {
+  console.log("clicked");
+});
+async function fetchData(query = "") {
+  const res = await fetch(`https://api.github.com/users/${query}`);
+  const data = await res.json();
+  displayData(data);
+  console.log(data)
+}
+
+fetchData();
+
+function displayData(data) {
+  let card = document.querySelector(".card");
+  if (data) {
+    card.innerHTML = `<div class="imgContainer">
+    <img src=${data.avatar_url} alt="user ">
+    </div>
+    <div class="info">
+    <p class="username">${data.login}</p>
+    <p class="bio">${data.bio}</p>
+    <div class="stats">
+        <p><span>${data.followers}</span>Followers</p>
+        <div class="circle"></div>
+        <p><span>${data.following}</span>Followings</p>
+        <div class="circle"></div>
+        <p><span>${data.public_repos}</span>Repositories</p>
+    </div>
+    </div>`;
+  } else {
+    return;
+  }
+}
